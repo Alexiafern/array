4b8ad0b7c258cae1863320e0125da529353aeb8a
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-02-28 16:28:34 -0300

    a

--- alexia.php
+++ alexia.php
@@ -4,26 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pergunta 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css" >
+    <title>trabalho do Dieimes</title>
 </head>
 <body>
-    <!-- Centralizar no css-->
-    <div class="inicio">
         <h1>O que é concatenação no PHP?</h1>
-    </div>
-    <P>O operador de concatenação de strings é o ponto .</P>
 
-    <P>Por exemplo:</P>
+        <div class="box">O operador de concatenação de strings é o ponto .</div>
 
-    <P>$ddd  = '34';</P>
-    <P>$fone = '32105691';</P>
+            <br>
 
-    <P>echo '(' . $ddd . ') ' . $fone;</P>
-    <P>Esse exemplo funciona igual com aspas duplas:</P>
+            <P>Por exemplo:</P>
 
-    <P>echo "(" . $ddd . ") " . $fone;</P>
-    <P>É assim que você concatena strings em PHP.</P>
-    <h2>Exemplos de concatenação em PHP:</h2>
+            <P>$ddd  = '34';</P>
+            <P>$fone = '32105691';</P>
+
+            <P>echo '(' . $ddd . ') ' . $fone;</P>
+            <P>Esse exemplo funciona igual com aspas duplas:</P>
+
+            <P>echo "(" . $ddd . ") " . $fone;</P>
+            <P>É assim que você concatena strings em PHP.</P>
+            <h2>Exemplos de concatenação em PHP:</h2>
     
+                    $a = "Hello ";
+                    $b = $a . "World!";
+                    $a = "Hello ";
+                    $a .= "World!";...
+                    
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </html>
